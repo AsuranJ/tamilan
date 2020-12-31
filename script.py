e57a9d3e8feb7c1b7aa134b3da2d65bc9ab5a837
AsuranJ <71362075+AsuranJ@users.noreply.github.com>
2020-12-31 09:47:22 +0530

    Update script.py

--- script.py
+++ script.py
@@ -19,8 +19,19 @@ class script(object):
     CUSTOM_CAPTION_UL_FILE = " "
     REPLY_TO_DOC_FOR_C2V = "Reply to a Telegram media to convert"
     REPLY_TO_DOC_FOR_RENAME_FILE = "Reply to a Telegram media"
-    HELP_USER = """It's not that complicated😅
+    HELP_USER = """Hey.. It's not that complicated😅
+
+Follow These steps..
     
-1. Send me any Telegram File.
-2. Choose appropriate option."""
+🌀 Send an image and it will be saved as your permanent Thumbnail
+
+🌀 Now send your file
+
+🌀 Select rename option and enter your new name ( extension is not required )
+
+NOTE : You must join our channel in order to USE ME
+
+Channel: @SerialCoIn
+
+"""
 
